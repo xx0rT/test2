e183a6ef612d37c68f70c6e6d7209bad06c044ec
Troxx <121587043+xx0rT@users.noreply.github.com>
2025-10-22 21:38:22 +0200

    Updated Hero.tsx

--- container/home-page/Hero.tsx
+++ container/home-page/Hero.tsx
@@ -31,7 +31,7 @@ export default function Hero() {
 										<Image
 											width={120}
 											height={50}
-											src="/image copy.png"
+											src="/image copy copy.png"
 											alt="img"
 											className="w-auto h-[95px] lg:w-auto lg:h-auto md:w-[100px] md:h-[63px] sm:w-[74px] sm:h-[45px] xm:w-[64px] xm:h-[40px] object-cover xl:mt-[15px] mt-[10px] rounded-[10px]"
 										/>
