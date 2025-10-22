fb9eccc305f050eb15774751b5a19dc60164165d
Troxx <121587043+xx0rT@users.noreply.github.com>
2025-10-22 21:36:10 +0200

    Updated background.png

--- container/services-page/Expectations.tsx
+++ container/services-page/Expectations.tsx
@@ -16,7 +16,7 @@ export default function Expectations() {
 		<section className="w-full bg-marquee padding-y rounded-t-[20px]">
 			<div className="w-full bg-marquee z-10 relative rounded-t-[20px]">
 				<Marquee
-					title="why us other"
+					title="proč my?"
 					className="pb-[50px] lg:pb-[40px] md:pb-[30px] sm:pb-[30px] xm:pb-[15px] text-[540px] leading-[330px] lg:text-[380px] lg:leading-[240px] md:text-[300px] md:leading-[160px] sm:text-[230px] sm:leading-[140px] xm:text-[130px] xm:leading-[80px]"
 				/>
 			</div>
