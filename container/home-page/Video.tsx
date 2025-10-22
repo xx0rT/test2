9a34aeadffa49c5ba768afc5e4a709eb43adfdc7
Troxx <121587043+xx0rT@users.noreply.github.com>
2025-10-22 08:28:01 +0200

    Updated Video.tsx

--- container/home-page/Video.tsx
+++ container/home-page/Video.tsx
@@ -1,5 +1,36 @@
-import { PlayVideo } from "@/components";
+import Image from "next/image";
+import { useRef } from "react";
+import { useScroll, useTransform, motion } from "framer-motion";
 
 export default function Video() {
-	return <PlayVideo videosrc="/homevideo.mp4" />;
+	const container = useRef(null);
+
+	const { scrollYProgress } = useScroll({
+		target: container,
+		offset: ["start end", "end start"],
+	});
+
+	const y = useTransform(scrollYProgress, [0, 1], [0, -100]);
+
+	return (
+		<div
+			className="w-full relative overflow-hidden"
+			ref={container}>
+			<motion.div
+				className="w-full h-full"
+				style={{ y }}
+				data-scroll
+				data-scroll-speed="-.8"
+				data-scroll-section>
+				<Image
+					src="/image.png"
+					alt="Vila Adalbert"
+					width={1920}
+					height={1080}
+					className="w-full h-full object-cover"
+					priority
+				/>
+			</motion.div>
+		</div>
+	);
 }
